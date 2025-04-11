d4535de8c4e9a62ad72693865b4e23be7d921357
Haseeb <iam-mhaseeb@users.noreply.github.com>
2025-04-12 09:44:12 +1000

    Initial Commit

---
+++ BreakReminder/BreakReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakReminderApp.swift
+//  BreakReminder
+//
+//  Created by Muhammad Haseeb on 12/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct BreakReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreakReminderTests/BreakReminderTests.swift
@@ -0,0 +1,16 @@
+//
+//  BreakReminderTests.swift
+//  BreakReminderTests
+//
+//  Created by Muhammad Haseeb on 12/04/2025.
+//
+
+import Testing
+
+struct BreakReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
